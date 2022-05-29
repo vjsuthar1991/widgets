94d98194c79f44cdec20f0b6bc82e34f4569a63e
vjsuthar1991 <suthar67@gmail.com>
2022-05-29 11:24:24 +0530

    Created widgets for google translate, Wikipedia search and accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const items = [
+        {
+            title: 'What is React 1?',
+            content: 'React is a front end javascript framework'
+        },
+        {
+            title: 'What is React 2?',
+            content: 'React is a front end javascript framework'
+        },
+        {
+            title: 'What is React 3?',
+            content: 'React is a front end javascript framework'
+        },
+        {
+            title: 'What is React 4?',
+            content: 'React is a front end javascript framework'
+        }
+    ];
+
+    const options = [
+        {
+            label: 'The Color Red',
+            value: 'red'
+        },
+        {
+            label: 'The Color Green',
+            value: 'green'
+        },
+        {
+            label: 'The shade of Blue',
+            value: 'blue'
+        },
+    ];
+    const [selected,setSelected] = useState(options[0]);
+    return (
+        <div>
+            {/* <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options}
+            /> */}
+            <Translate />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Dropdown.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Dropdown = ({ label,options,selected,onSelectedChange }) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+            if(ref.current.contains(event.target)){
+                return
+            }
+            setOpen(false);
+        });
+    }, []);
+    const renderedOptions = options.map((option)=>{
+        if(option.value === selected.value){
+            return null;
+        }
+        return (
+            <div key={option.value} className="item" onClick={()=>onSelectedChange(option)}>
+                {option.label}
+            </div>
+        );
+    });
+
+    return (
+        <div ref={ref} className="ui form">
+            <div className="field">
+                <label className="label">{label}</label>
+                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+                    <i className="dropdown icon"></i>
+                    <div className="text"><h1>{selected.label}</h1></div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dropdown;
